9d29ba347cb5724b861bb2aaef3efe1583cf0134
lonewolfyx <877085893@qq.com>
2025-04-18 13:57:12 +0800

    fix: 版本定义

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as process from 'node:process'
 import { Logger } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { env } from '@/utils/env'
+import { version } from '../package.json'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
@@ -14,7 +15,7 @@ async function bootstrap() {
 	const config = new DocumentBuilder()
 		.setTitle(env('PROJECT_TITLE'))
 		.setDescription(env('PROJECT_KEYWORD'))
-		.setVersion('1.0')
+		.setVersion(version)
 		.build()
 	const documentFactory = () => SwaggerModule.createDocument(app, config)
 	SwaggerModule.setup('api', app, documentFactory, {
