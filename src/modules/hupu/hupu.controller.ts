335c0e0e899b6b18879205d8db9885755274db3d
bin <962920136@qq.com>
2025-04-18 10:08:42 +0800

    feat: 新增虎扑 swagger tag

--- src/modules/hupu/hupu.controller.ts
+++ src/modules/hupu/hupu.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { HupuService } from './hupu.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 
 @Controller('hupu')
+@ApiTags('虎扑')
 export class HupuController {
     constructor(private readonly hupuService: HupuService) {}
 
     @Get('hot')
+    @ApiOperation({ summary: '虎扑热榜' })
     async getHupuHot(@Query('type') type: 'basketball' | 'football' | 'street') {
         return this.hupuService.getHupuHot(type)
     }
