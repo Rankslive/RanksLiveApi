05f9deebe20698402eba94f968d4c7b729517b54
lonewolfyx <877085893@qq.com>
2025-04-20 10:51:39 +0800

    feat: 新增哔哩哔哩 综艺、动画、游戏、鬼畜、音乐、舞蹈、影视、娱乐、知识、科技数码、美食、汽车、时尚美妆、体育运动、动物榜单

--- src/modules/bilibili/bilibili.controller.ts
+++ src/modules/bilibili/bilibili.controller.ts
@@ -46,7 +46,97 @@ export class BilibiliController {
 
 	@Get('rank/tv')
 	@ApiOperation({ summary: '获取哔哩哔哩 · 电视剧排行榜' })
-	async gettvRank() {
+	async getTvRank() {
 		return await this.BiliBiliService.getSeasonRankList(BiliBiliRankType.tv)
+	}
+
+	@Get('rank/variety')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 综艺排行榜' })
+	async getVarietyRank() {
+		return await this.BiliBiliService.getSeasonRankList(BiliBiliRankType.variety)
+	}
+
+	@Get('rank/douga')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 动画排行榜' })
+	async getDouGaRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.douga)
+	}
+
+	@Get('rank/game')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 游戏排行榜' })
+	async getGameRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.game)
+	}
+
+	@Get('rank/kichiku')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 鬼畜排行榜' })
+	async getKichikuRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.kichiku)
+	}
+
+	@Get('rank/music')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 音乐排行榜' })
+	async getMusicRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.music)
+	}
+
+	@Get('rank/dance')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 舞蹈排行榜' })
+	async getDanceRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.dance)
+	}
+
+	@Get('rank/cinephile')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 影视排行榜' })
+	async getCinephileRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.cinephile)
+	}
+
+	@Get('rank/ent')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 娱乐排行榜' })
+	async getEntRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.ent)
+	}
+
+	@Get('rank/knowledge')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 知识排行榜' })
+	async getKnowledgeRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.knowledge)
+	}
+
+	@Get('rank/tech')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 科技数码排行榜' })
+	async getTechRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.tech)
+	}
+
+	@Get('rank/food')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 美食排行榜' })
+	async getFoodRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.food)
+	}
+
+	@Get('rank/car')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 汽车排行榜' })
+	async getCarRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.car)
+	}
+
+	@Get('rank/fashion')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 时尚美妆排行榜' })
+	async getFashionRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.fashion)
+	}
+
+	@Get('rank/sports')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 体育运动排行榜' })
+	async getSportsRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.sports)
+	}
+
+	@Get('rank/animal')
+	@ApiOperation({ summary: '获取哔哩哔哩 · 动物排行榜' })
+	async getAnimalRank() {
+		return await this.BiliBiliService.getRankListV2(BiliBiliRankType.animal)
 	}
 }
--- src/modules/bilibili/bilibili.service.ts
+++ src/modules/bilibili/bilibili.service.ts
@@ -7,156 +7,93 @@ import { BiliBiliSeasonType } from '@/types/bilibili'
 
 @Injectable()
 export class BilibiliService {
-	async getComprehensiveHotRankList(): Promise<ResponseData[] & []> {
-		const { data } = await request({
-			method: 'get',
-			url: 'https://api.bilibili.com/x/web-interface/ranking/v2',
-			headers: {
-				'User-Agent': BASE_USER_AGENT
-			}
-		})
+	private async sendRequest(url: string, queryParams: any) {
+		try {
+			const query = await encodeRid(queryParams)
 
+			const { data } = await request({
+				method: 'get',
+				url,
+				headers: {
+					'User-Agent': BASE_USER_AGENT
+				},
+				params: query
+			})
+
+			return data
+		} catch (error) {
+			console.error(`请求 ${url} 时出错:`, error)
+			return null
+		}
+	}
+
+	// 封装数据处理逻辑
+	private processData(data: any): ResponseData[] {
 		if (!data) {
 			return []
 		}
 
-		return data.data?.list.map((item: any) => {
-			return {
-				title: item.title,
-				view: item.stat.view,
+		const list = data.data?.list || data.result?.list
 
-				url: `https://www.bilibili.com/video/${item.bvid}`,
-				create_time: item.ctime
-			}
-		})
+		return (
+			list?.map((item: any) => {
+				return {
+					title: item.title,
+					view: item.stat.view,
+					url: item.url ? item.url : `https://www.bilibili.com/video/${item.bvid}`,
+					create_time: item.ctime || 0
+				}
+			}) || []
+		)
 	}
 
-	async getPopularPreciousRankList(): Promise<ResponseData[] & []> {
+	async getComprehensiveHotRankList(): Promise<ResponseData[]> {
+		const data = await this.sendRequest('https://api.bilibili.com/x/web-interface/ranking/v2', {})
+		return this.processData(data)
+	}
+
+	async getPopularPreciousRankList(): Promise<ResponseData[]> {
 		const queryParams = {
 			page_size: 100,
 			page: 1,
-
 			web_location: 333.934
 		}
 
-		const query = await encodeRid(queryParams)
+		const data = await this.sendRequest('https://api.bilibili.com/x/web-interface/popular/precious', queryParams)
 
-		const { data } = await request({
-			method: 'get',
-			url: 'https://api.bilibili.com/x/web-interface/popular/precious',
-			headers: {
-				'User-Agent': BASE_USER_AGENT
-			},
-			params: query
-		})
-
-		if (!data) {
-			return []
-		}
-
-		return data.data?.list.map((item: any) => {
-			return {
-				title: item.title,
-				view: item.stat.view,
-				url: `https://www.bilibili.com/video/${item.bvid}`,
-				create_time: item.ctime
-			}
-		})
+		return this.processData(data)
 	}
 
-	async getWebRankList(type: BiliBiliSeasonType) {
+	async getWebRankList(type: BiliBiliSeasonType): Promise<ResponseData[]> {
+		const queryParams = {
+			day: 3,
+			season_type: type,
+			web_location: 333.934
+		}
+		const data = await this.sendRequest('https://api.bilibili.com/pgc/web/rank/list', queryParams)
+		return this.processData(data)
+	}
+
+	async getSeasonRankList(type: BiliBiliSeasonType): Promise<ResponseData[]> {
 		const queryParams = {
 			day: 3,
 			season_type: type,
 			web_location: 333.934
 		}
 
-		const query = await encodeRid(queryParams)
+		const data = await this.sendRequest('https://api.bilibili.com/pgc/season/rank/web/list', queryParams)
 
-		const { data } = await request({
-			method: 'get',
-			url: 'https://api.bilibili.com/pgc/web/rank/list',
-			headers: {
-				'User-Agent': BASE_USER_AGENT
-			},
-			params: query
-		})
-
-		if (!data) {
-			return []
-		}
-
-		return data?.result?.list.map((item: any) => {
-			return {
-				title: item.title,
-				view: item.stat.view,
-				url: item.url,
-				create_time: 0
-			}
-		})
+		return this.processData(data)
 	}
 
-	async getSeasonRankList(type: BiliBiliSeasonType) {
-		const queryParams = {
-			day: 3,
-			season_type: type,
-			web_location: 333.934
-		}
-
-		const query = await encodeRid(queryParams)
-
-		const { data } = await request({
-			method: 'get',
-			url: 'https://api.bilibili.com/pgc/season/rank/web/list',
-			headers: {
-				'User-Agent': BASE_USER_AGENT
-			},
-			params: query
-		})
-
-		if (!data) {
-			return []
-		}
-
-		return data?.data?.list.map((item: any) => {
-			return {
-				title: item.title,
-				view: item.stat.view,
-				url: item.url,
-				create_time: 0
-			}
-		})
-	}
-
-	async getRankListV2(type: BiliBiliSeasonType) {
+	async getRankListV2(type: BiliBiliSeasonType): Promise<ResponseData[]> {
 		const queryParams = {
 			rid: type,
 			type: 'all',
 			web_location: 333.934
 		}
+		const data = await this.sendRequest('https://api.bilibili.com/x/web-interface/ranking/v2', queryParams)
 
-		const query = await encodeRid(queryParams)
-
-		const { data } = await request({
-			method: 'get',
-			url: 'https://api.bilibili.com/x/web-interface/ranking/v2',
-			headers: {
-				'User-Agent': BASE_USER_AGENT
-			},
-			params: query
-		})
-
-		if (!data) {
-			return []
-		}
-
-		return data?.data?.list.map((item: any) => {
-			return {
-				title: item.title,
-				view: item.stat.view,
-				url: item.url,
-				create_time: 0
-			}
-		})
+		return this.processData(data)
 	}
 }
