b117ee6c28c8fac3c9119c21a064006df458b84b
lonewolfyx <877085893@qq.com>
2025-05-06 19:13:51 +0800

    feat: 添加维护者修饰器

---
+++ src/common/decorator/api.maintainers.decorator.ts
@@ -0,0 +1,8 @@
+import { ApiExtension } from '@nestjs/swagger'
+
+/**
+ * 添加维护者修饰器
+ * @param authors github 上的维护者用户名，即：https://github.com/{owner}
+ * @constructor
+ */
+export const ApiMaintainers = (authors: string | string[]) => ApiExtension('x-maintainers', authors)
