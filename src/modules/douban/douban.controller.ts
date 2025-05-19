3d779ea2e0f77a9c08fe25170b7247024492b22f
lonewolfyx <877085893@qq.com>
2025-05-19 17:17:14 +0800

    feat: 新增豆瓣华语口碑剧集榜

--- src/modules/douban/douban.controller.ts
+++ src/modules/douban/douban.controller.ts
@@ -5,6 +5,7 @@ import { DoubanConstants } from '@/modules/douban/constants/douban.constants'
 import { DouBanParamDto } from '@/modules/douban/douban.dto'
 import { ApiMaintainers } from '@/common/decorator/api.maintainers.decorator'
 import { SourceUrl } from '@/common/decorator/source.url.decorator'
+import { ApiDescription } from '@/common/decorator/api.description.decorator'
 
 @Controller('douban')
 @ApiTags('豆瓣')
@@ -12,11 +13,19 @@ export class DoubanController {
     constructor(private readonly doubanService: DoubanService) {}
 
     @Get('real/:type')
-    @ApiOperation({ summary: '获取豆瓣榜单' })
+    @ApiOperation({ summary: '获取豆瓣榜单 · 书影音、电影、电视榜' })
     @ApiMaintainers('lonewolfyx')
     @SourceUrl('https://m.douban.com/subject_collection/subject_real_time_hotest')
     async getSubjectRank(@Param() param: DouBanParamDto): Promise<any> {
         const { type } = param
         return await this.doubanService.getRealRankList(DoubanConstants[type]['value'])
+    }
+
+    @Get('tv_chinese')
+    @ApiDescription('获取豆瓣华语口碑剧集榜')
+    @ApiMaintainers('lonewolfyx')
+    @SourceUrl('https://m.douban.com/subject_collection/tv_chinese_best_weekly')
+    async getTvChineseRank() {
+        return await this.doubanService.getTvChineseRank()
     }
 }
--- src/modules/douban/douban.service.ts
+++ src/modules/douban/douban.service.ts
@@ -3,6 +3,8 @@ import { BASE_USER_AGENT } from '@/constants/base.constants'
 import { douBanType } from './types/douban'
 import { HttpClientService } from '@/common/service/http-client.service'
 
+const DOUBAN_APP_USERAGENT = 'api-client/0.1.3 com.douban.frodo/7.100.0 iOS/18.0 model/iPad8,6 network/wifi'
+
 // 数据源 https://m.douban.com/home_guide
 
 @Injectable()
@@ -42,5 +44,35 @@ export class DoubanService {
                 create_time: 0
             }
         })
+    }
+
+    async getTvChineseRank() {
+        const { data } = await this.httpClientService.request({
+            method: 'get',
+            url: 'https://m.douban.com/rexxar/api/v2/subject_collection/tv_chinese_best_weekly/items',
+            params: {
+                start: 0,
+                count: 10,
+                updated_at: '',
+                items_only: 1,
+                type_tag: '',
+                for_mobile: 1
+            },
+            headers: {
+                'User-Agent': DOUBAN_APP_USERAGENT,
+                referer: 'https://m.douban.com/subject_collection/tv_chinese_best_weekly'
+            }
+        })
+
+        return (
+            data?.subject_collection_items.map((item: any) => {
+                return {
+                    title: item.title,
+                    view: item.rating.count,
+                    url: item.url,
+                    create_time: 0
+                }
+            }) || []
+        )
     }
 }
