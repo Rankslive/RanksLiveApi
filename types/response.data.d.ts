f3c698f201d248c65b9a83267721b1f56c107448
lonewolfyx <877085893@qq.com>
2025-05-20 22:45:08 +0800

    fix: 优化响应类型

--- types/response.data.d.ts
+++ types/response.data.d.ts
@@ -1,6 +1,14 @@
 export interface ResponseData {
+    // 标题
     title: string
+    // 次数 ｜ 统计总数
     view: number | string
+    // 地址
     url: string
+    // 创建时间
     create_time: number
+    // 封面
+    thumbnail?: string
+    // 描述
+    description?: string
 }
