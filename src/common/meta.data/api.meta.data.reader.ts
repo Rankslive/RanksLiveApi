062ae6a1bb45686c94dd2702cf558a6b3018c110
lonewolfyx <877085893@qq.com>
2025-05-24 16:44:50 +0800

    feat: 新增 api meta data 阅读器

---
+++ src/common/meta.data/api.meta.data.reader.ts
@@ -0,0 +1,15 @@
+import { ExecutionContext, Injectable } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+
+@Injectable()
+export class ApiMetadataReader {
+    constructor(private readonly reflector: Reflector) {
+    }
+
+    getMetadata(context: ExecutionContext) {
+        const handler = context.getHandler()
+        const controller = context.getClass()
+
+        return (key: string) => this.reflector.getAllAndOverride<string>(key, [handler, controller])
+    }
+}
