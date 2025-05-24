d8e9ab7b0c51ac5523aace4fbd0445e26690767a
lonewolfyx <877085893@qq.com>
2025-05-24 16:44:25 +0800

    fix: 优化百度更换修饰器使用

--- src/modules/baidu/baidu.controller.ts
+++ src/modules/baidu/baidu.controller.ts
@@ -3,21 +3,23 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { BaiduService } from '@/modules/baidu/baidu.service'
 import { ApiMaintainers } from '@/common/decorator/api.maintainers.decorator'
 import { SourceUrl } from '@/common/decorator/source.url.decorator'
+import { ApiDescription } from '@/common/decorator/api.description.decorator'
+import { ApiPlatform } from '@/common/decorator/api.platform.decorator'
 
 @Controller('baidu')
-@ApiTags('百度')
+@ApiPlatform('百度')
 export class BaiduController {
     constructor(private BaiduService: BaiduService) {}
 
     @Get('hot')
-    @ApiOperation({ summary: '获取百度热搜' })
+    @ApiDescription('获取百度热搜')
     @ApiMaintainers('lonewolfyx')
     @SourceUrl('https://top.baidu.com/board?platform=pc&sa=pcindex_entry')
     async getBaiduHot() {
         return await this.BaiduService.getBaiduHot('realtime')
     }
     @Get('tieba/discussion')
-    @ApiOperation({ summary: '获取百度贴吧热议榜' })
+    @ApiDescription('获取百度贴吧热议榜')
     @ApiMaintainers('lonewolfyx')
     @SourceUrl('https://tieba.baidu.com/hottopic/browse/topicList?res_type=1')
     async getBaiDuTieBaDiscussion() {
