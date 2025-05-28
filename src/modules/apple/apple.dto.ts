527f7b5210c27c56ccc7d9e2a679dc7a485bb2e1
lonewolfyx <877085893@qq.com>
2025-05-28 14:53:04 +0800

    fix: 优化公用dto参数继承

--- src/modules/apple/apple.dto.ts
+++ src/modules/apple/apple.dto.ts
@@ -12,33 +12,23 @@ import { validateMessage } from '@/utils/validation.prompts'
 import { AppleCategory, AppleCountry, AppleRankList } from '@/modules/apple/constants/apple.constants'
 import { AppleMusicDaily, AppleMusicGenres } from '@/modules/apple/constants/apple.music.constants'
 
-export class AppleAppStoreParamDto {
-    // @ApiPropertyEnumExtend('操作系统类型 ios、mac', AppleSystem, 'ios')
-    // @IsEnum(transformConstantsObjectToEnum(AppleSystem), { message: validateMessage('system') })
-    // system: AppleSystemType
+export class AppleCountryParamDto {
+    @ApiPropertyEnumExtend('国家归属', AppleCountry, 'us')
+    @IsEnum(transformConstantsObjectToEnum(AppleCountry), { message: validateMessage('country') })
+    country: AppleCountryType
+}
 
+export class AppleAppStoreParamDto extends AppleCountryParamDto {
     @ApiPropertyEnumExtend('榜单类型', AppleRankList, 'free')
     @IsEnum(transformConstantsObjectToEnum(AppleRankList), { message: validateMessage('rankType') })
     rankType: AppleRankType
 
-    @ApiPropertyEnumExtend('国家归属', AppleCountry, 'us')
-    @IsEnum(transformConstantsObjectToEnum(AppleCountry), { message: validateMessage('country') })
-    country: AppleCountryType
-
     @ApiPropertyEnumExtend('app、游戏分类', AppleCategory, 'app-sport')
     @IsEnum(transformConstantsObjectToEnum(AppleCategory), { message: validateMessage('category') })
     category: AppleCategoryType
-
-    // @ApiPropertyEnumExtend('设备', AppleDevice, 'iphone')
-    // @IsEnum(transformConstantsObjectToEnum(AppleDevice), { message: validateMessage('device') })
-    // device: AppleDeviceType
 }
 
-export class AppleMusicParamDto {
-    @ApiPropertyEnumExtend('国家归属', AppleCountry, 'us')
-    @IsEnum(transformConstantsObjectToEnum(AppleCountry), { message: validateMessage('country') })
-    country: AppleCountryType
-
+export class AppleMusicParamDto extends AppleCountryParamDto {
     @ApiPropertyEnumExtend('音乐类型', AppleMusicGenres, 'music')
     @IsEnum(transformConstantsObjectToEnum(AppleMusicGenres), { message: validateMessage('type') })
     type: AppleMusicType
