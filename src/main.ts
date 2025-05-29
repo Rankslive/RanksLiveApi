0f446162aa829f0607c5f55177a59303ca8976d0
lonewolfyx <877085893@qq.com>
2025-05-29 19:19:26 +0800

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { version } from '../package.json'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true })
+    const app = await NestFactory.create(AppModule)
 
     // 跨域
     app.enableCors()
