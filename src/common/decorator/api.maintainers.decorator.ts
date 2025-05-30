574056a3aa1d6c2ae10d801c44672bf1be9d01a9
lonewolfyx <877085893@qq.com>
2025-05-30 17:12:15 +0800

    fix: default support array contributors

--- src/common/decorator/api.maintainers.decorator.ts
+++ src/common/decorator/api.maintainers.decorator.ts
@@ -1,11 +1,12 @@
 import { ApiExtension } from '@nestjs/swagger'
+import { isArray } from 'radash'
 
 /**
  * 添加维护者修饰器
  * @param authors github 上的维护者用户名，即：https://github.com/{owner}
  * @constructor
  */
-export const ApiMaintainers = (authors: string | string[]) => ApiExtension('x-maintainers', authors)
+export const ApiMaintainers = (authors: string | string[]) => ApiExtension('x-maintainers', !isArray(authors) ? [authors] : authors)
 
 // export const ApiMaintainers = (authors: string | string[]) => ApiExtension('x-maintainers', {
 //     authors,
